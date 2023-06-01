86e0e44776426a170659c0afc1a344c1ea9e2886
Ali <Ali@ALI-DESKTOP>
2023-06-01 14:39:13 +0300

    fix adds

--- app/src/main/java/com/pakachu/quickrm/MainActivity.java
+++ app/src/main/java/com/pakachu/quickrm/MainActivity.java
@@ -4,14 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -19,18 +15,19 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
-import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.FullScreenContentCallback;
 import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.ads.interstitial.InterstitialAd;
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
@@ -119,38 +116,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {}
+        });
+
         AdView mAdView = findViewById(R.id.adView2);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
-        new CountDownTimer(60000, 1000) {
-            public void onTick(long millisUntilFinished) {
-                Log.e("timer",""+millisUntilFinished);
-            }
+        AdRequest adRequest2 = new AdRequest.Builder().build();
+        InterstitialAd.load(MainActivity.this, getResources().getString(R.string.interstatialAds), adRequest2,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        mInterstitialAd = interstitialAd;
 
-            public void onFinish() {
-                AdRequest adRequest2 = new AdRequest.Builder().build();
-                InterstitialAd.load(MainActivity.this, getResources().getString(R.string.interstatialAds), adRequest2,
-                        new InterstitialAdLoadCallback() {
+                        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
                             @Override
-                            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
-                                mInterstitialAd = interstitialAd;
-                                if (mInterstitialAd != null) {
-                                    mInterstitialAd.show(MainActivity.this);
-                                }
+                            public void onAdClicked() {
+                                // Called when a click is recorded for an ad.
+                                Log.d("add", "Ad was clicked.");
                             }
 
                             @Override
-                            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                            public void onAdDismissedFullScreenContent() {
+                                // Called when ad is dismissed.
+                                // Set the ad reference to null so you don't show the ad a second time.
+                                Log.d("add", "Ad dismissed fullscreen content.");
                                 mInterstitialAd = null;
                             }
+
+                            @Override
+                            public void onAdFailedToShowFullScreenContent(AdError adError) {
+                                // Called when ad fails to show.
+                                Log.e("add", "Ad failed to show fullscreen content.");
+                                mInterstitialAd = null;
+                            }
+
+                            @Override
+                            public void onAdImpression() {
+                                // Called when an impression is recorded for an ad.
+                                Log.d("add", "Ad recorded an impression.");
+                            }
+
+                            @Override
+                            public void onAdShowedFullScreenContent() {
+                                // Called when ad is shown.
+                                Log.d("add", "Ad showed fullscreen content.");
+                            }
                         });
-            }
-        }.start();
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        mInterstitialAd = null;
+                    }
+                });
 
         birim = getResources().getString(R.string.maks);
-
-
         Button btn_pro = findViewById(R.id.btn_pro);
         btn_pro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -228,11 +252,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 hesapla();
+
+                if (mInterstitialAd != null) {
+                    mInterstitialAd.show(getParent());
+                } else {
+                    Log.d("add", "The interstitial ad wasn't ready yet.");
+                }
             }
         });
 
-        Button diger = findViewById(R.id.btn_diger);
-        diger.setOnClickListener(new View.OnClickListener() {
+        Button btn_diger = findViewById(R.id.btn_diger);
+        btn_diger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(GoToStore());
